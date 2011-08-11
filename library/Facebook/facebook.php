fc6a07b194352b2ec8d1769c0da6bfbfc760c0b7
Joshua Johanan <josh359@gmail.com>
2011-08-11 14:02:30 -0400

    Adding the first files

---
+++ library/Facebook/facebook.php
@@ -0,0 +1,7 @@
+require_once "base_facebook.php";
+
+/**
+ * Extends the BaseFacebook class with the intent of using
+ * PHP sessions to store user ids and access tokens.
+ */
+class Facebook_Facebook extends BaseFacebook
---
+++ library/Josh/facebook.php
@@ -0,0 +1,43 @@
+<?php
+
+class Josh_Facebook
+{
+	
+	private static $fb;
+	
+	private static function getFB()
+	{
+		if(self::$fb)
+		{
+			return self::$fb;
+		}
+		
+		$bootstrap = Zend_Controller_Front::getInstance()->getParam('bootstrap');
+		
+		$options = $bootstrap->getOptions();
+		
+		
+		$fb = New Facebook_Facebook(array(
+				'appId' => $options['facebook']['appid'],
+				'secret' => $options['facebook']['appsecret'],
+				));
+				
+		$fb->setAccessToken($_COOKIE['fb_token']);
+		self::$fb = $fb;
+		
+		return self::$fb;
+	}
+	
+	public static function __callStatic ( $name, $args ) 
+	{
+
+        $callback = array ( self::getFB(), $name ) ;
+        return call_user_func_array ( $callback , $args ) ;
+    }
+	
+	public static function viewFB()
+	{
+		return self::getFB();
+	}
+}
+?>
